938f9d5e0a1fbed2e556b79c5e3ed8aa0f3349ab
Joseph Snell <joepsnell@gmail.com>
2020-06-10 06:32:26 -0500

    fix typo

--- test/nrql-alert.test.ts
+++ test/nrql-alert.test.ts
@@ -1,7 +1,7 @@
 import { expect, exactlyMatchTemplate } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 
-it('nrql-alert', () => {
+test('nrql-alert', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, 'my-stack');
   expect(stack).to(exactlyMatchTemplate({
